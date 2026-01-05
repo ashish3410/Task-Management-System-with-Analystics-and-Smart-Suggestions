758d8b87cb8803651a37fc8ac290a4465c5f3dd6
ashish3410 <aashishklko@gmail.com>
2026-01-06 00:07:17 +0530

    change for deployment

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+
+const isDevelopment = import.meta.env.MODE === 'development';
+const mybaseUrl = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOY;
 const API=axios.create({
-    baseURL:'http://127.0.0.1:8000/api/',
+    baseURL: mybaseUrl,
 });
 API.interceptors.response.use(
   res => res,
